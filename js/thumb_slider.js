4c822a083a40000eae46f3f8f83d67aef45dac5b
orachun <orachun_chun@hotmail.com>
2013-09-16 00:03:54 +0800

    none

--- js/thumb_slider.js
+++ js/thumb_slider.js
@@ -59,10 +59,10 @@ function gallery(container_selector, img_selector)
 	$(container_selector).each(function(){
 		var container = $(this);
 		if(container.hasClass('gallery-inited')) return;
-		
-		container.append('<div class="gallery-viewport ui-corner-all"></div>');
+		container.append('<div class="gallery-controller"><span class="prev-btn"/><span class="next-btn"/></div>');
+		container.append('<div class="gallery-viewport ui-corner-all"><div style="width: inherit; height: interit;display: table-cell;vertical-align: middle; text-align: center;"/></div>');
 		container.append('<div class="gallery-thumb-list"></div>');
-		var viewport = container.find('.gallery-viewport');
+		var viewport = container.find('.gallery-viewport > div');
 		var thumb_list = container.find('.gallery-thumb-list');
 		container.find(img_selector).each(function(i, e){
 			
@@ -89,26 +89,47 @@ function gallery(container_selector, img_selector)
 			thumb_list.append(e);
 			$(e).addClass('gallery-thumb');
 			$(e).attr('forindex', i);
-			
 		});
-		thumb_list.find('.gallery-thumb').wrap('<div style="display:inline-block;"><div class="gallery-thumb-container"/></div>')
+		thumb_list.find('.gallery-thumb').wrap('<div style="display:inline-block;"><div class="gallery-thumb-container ui-corner-all"/></div>')
 		viewport.find('.gallery-image').hide(0);
 		viewport.find('.gallery-image-0').addClass('gallery-current-image');
 		viewport.find('.gallery-image-0').show(0);
 		thumb_list.find('.gallery-thumb-container:first').addClass('gallery-current-thumb');
-		thumb_list.find('.gallery-thumb-container').click(function(){
-			var index = $(this).find('.gallery-thumb').attr('forindex');
-			var img = viewport.find('.gallery-image-'+index);
+		var show_image = function(index)
+		{
 			var current = viewport.find('.gallery-current-image');
+			var img = viewport.find('.gallery-image-'+index);
 			current.removeClass('gallery-current-image');
-			thumb_list.find('.gallery-current-thumb').removeClass('gallery-current-thumb');
-			var thumb = $(this);
+			var current_thumb = thumb_list.find('.gallery-current-thumb');
+			current_thumb.removeClass('gallery-current-thumb');
+			var thumb = thumb_list.find('.gallery-thumb[forindex="'+index+'"]');
+			thumb.parent().addClass('gallery-current-thumb');
 			current.hide('fade', 200, function(){
 				img.addClass('gallery-current-image');
 				img.show('fade', 200);
-				thumb.addClass('gallery-current-thumb');
 			});
-			
+		};
+		thumb_list.find('.gallery-thumb-container').click(function(){
+			var index = $(this).find('.gallery-thumb').attr('forindex');
+			show_image(index);
+		});
+		container.find('.gallery-controller').hide(0);
+		container.find('.gallery-viewport, .gallery-controller').hover(function(){
+			container.find('.gallery-controller').show(0);
+		}, function(){
+			container.find('.gallery-controller').hide(0);
+		});
+		var controller = container.find('.gallery-controller');
+		var total_images = thumb_list.find('.gallery-thumb').length;
+		controller.find('.prev-btn').click(function(){
+			var current_index = parseInt(thumb_list.find('.gallery-current-thumb .gallery-thumb').attr('forindex'));
+			var index = (total_images+current_index-1)%total_images;
+			show_image(index);
+		});
+		controller.find('.next-btn').click(function(){
+			var current_index = parseInt(thumb_list.find('.gallery-current-thumb .gallery-thumb').attr('forindex'));
+			var index = (current_index+1)%total_images;
+			show_image(index);
 		});
 		
 		container.addClass('gallery-inited');	
