6034c4b46b670c287e91ea1a599ad083c0048d50
orachun <orachun_chun@hotmail.com>
2013-10-13 15:50:16 +0800

    add tinymce and fixed slideshow

---
+++ js/admin.js
@@ -0,0 +1,23 @@
+function initEditor(selector)
+{
+	if (!$(selector).hasClass("editor-inited"))
+	{
+		tinymce.init({
+			selector: selector,
+			plugins: [
+				"advlist autolink autosave link image lists charmap hr anchor pagebreak spellchecker",
+				"searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+				"table contextmenu directionality emoticons template textcolor paste fullpage textcolor"
+			],
+			toolbar1: "forecolor backcolor bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | styleselect fontselect fontsizeselect | searchreplace | bullist numlist | outdent indent blockquote | link unlink anchor image media | table | hr removeformat | subscript superscript | charmap emoticons | fullscreen code",
+			menubar: false,
+			toolbar_items_size: 'small',
+			setup: function(editor) {
+				editor.on('change', function(e) {
+					editor.save();
+				});
+			}
+		});
+		$(selector).addClass("editor-inited");
+	}
+}
